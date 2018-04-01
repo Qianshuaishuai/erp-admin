183c60e78d586d99b99a237d9742af3c5f8215af
YuFeng <feng950704@foxmail.com>
2018-04-01 15:11:07 +0800

    finish question update

--- controllers/question.go
+++ controllers/question.go
@@ -3,6 +3,9 @@ package controllers
 import (
 	"dreamEbagPaperAdmin/models"
 	"dreamEbagPaperAdmin/helper"
+	"strings"
+	"strconv"
+	"regexp"
 )
 
 type QuestionController struct {
@@ -21,10 +24,8 @@ func (self *QuestionController) Detail() {
 	self.Data["ApiCss"] = true
 
 	questionId, _ := self.GetInt64("question_id", 0)
-	isBig, data := models.GetQuestion(questionId)
-
-	self.Data["IsBig"] = isBig
-	self.Data["Data"] = data
+	questionQ := self.GetString("question_q")
+	isBig, data := models.GetQuestion(questionId, questionQ)
 
 	if isBig {
 		bigQuestion := data.(models.BigQuestion)
@@ -37,17 +38,79 @@ func (self *QuestionController) Detail() {
 		if smallQuestion.RealType == models.OBJECTIVELY_BLANK || smallQuestion.RealType == models.SUBJECTIVITY_BLANK {
 			tempMap[models.OBJECTIVELY_BLANK] = "客观填空题"
 			tempMap[models.SUBJECTIVITY_BLANK] = "主观填空题"
+
+			//如果这道题是客观填空题，而且还没答案
+			if smallQuestion.RealType == models.OBJECTIVELY_BLANK {
+				if len(smallQuestion.RealAnswer) == 0 {
+					input := generateBlanks(smallQuestion.Content)
+					smallQuestion.RealAnswer = make([]string, input)
+					data = smallQuestion
+				}
+			}
 		}
 		self.Data["QuestionTypeMap"] = tempMap
 	}
 
+	self.Data["IsBig"] = isBig
+	self.Data["Data"] = data
 	self.display()
 }
 
+func generateBlanks(s string) int {
+	return len(regexp.MustCompile(`[\[<]/input[\]>]`).FindAllString(s, -1))
+}
+
 func (self *QuestionController) Edit() {
-	//QuestionId, _ := self.GetInt64("question_id")
-	//isBig, _ := self.GetBool("isBig")
-	//QuestionType, _ := self.GetInt("s_question_type")
-	//
-	//
+	questionId, _ := self.GetInt64("question_id")
+	isBig, _ := self.GetBool("isBig")
+	questionType, _ := self.GetInt("s_question_type")
+	data := make(map[string]interface{})
+
+	if isBig {
+		content := strings.TrimSpace(self.GetString("question_content"))
+		data["content"] = content
+
+	} else {
+		content := strings.TrimSpace(self.GetString("question_content"))
+		solution := strings.TrimSpace(self.GetString("s_question_solution"))
+		score, _ := self.GetFloat("s_question_score", -100)
+		difficulty, _ := self.GetFloat("s_question_difficulty", -100)
+
+		option_len, _ := self.GetInt("op_len", -1)
+		options := make(map[int]string)
+
+		if option_len != -1 {
+			for i := 0; i < option_len; i++ {
+				result := strings.TrimSpace(self.GetString("options" + strconv.Itoa(i)))
+				if len(result) > 0 {
+					options[i] = result
+				}
+			}
+		}
+
+		an_len, _ := self.GetInt("an_len", -1)
+		answers := make(map[int]string)
+
+		if an_len != -1 {
+			for i := 0; i < an_len; i++ {
+				result := strings.TrimSpace(self.GetString("answers" + strconv.Itoa(i)))
+				if len(result) > 0 {
+					answers[i] = result
+				}
+			}
+		}
+
+		data["content"] = content
+		data["solution"] = solution
+		data["score"] = score
+		data["difficulty"] = difficulty
+		data["options"] = options
+		data["answers"] = answers
+		data["an_len"] = an_len
+	}
+
+	if err := models.UpdateQuestion(questionId, isBig, questionType, data); err != nil {
+		self.ajaxMsg(err.Error(), -1)
+	}
+	self.ajaxMsg("", 0)
 }
--- models/mquestion.go
+++ models/mquestion.go
@@ -4,6 +4,9 @@ import (
 	"dreamEbagPaperAdmin/helper"
 	"encoding/json"
 	"strings"
+	"errors"
+	"bytes"
+	"regexp"
 )
 
 const (
@@ -79,12 +82,12 @@ type BigQuestion struct {
 	Content        string `gorm:"column:F_content;type:LONGTEXT" json:"content"` //问题的内容
 	BigQuestionIds string `gorm:"column:F_question_ids;type:TEXT" json:"-"`
 
-	RealType       int
-	Options       []string
-	RealAnswer    []string
-	Solution      string  `gorm:"column:F_solution;type:LONGTEXT" json:"solution"`
-	Score         float32 `gorm:"column:F_score;type:FLOAT(4,1)" json:"score"`
-	Difficulty    float64 `gorm:"column:F_difficulty;" json:"difficulty"`
+	RealType   int
+	Options    []string
+	RealAnswer []string
+	Solution   string  `gorm:"column:F_solution;type:LONGTEXT" json:"solution"`
+	Score      float32 `gorm:"column:F_score;type:FLOAT(4,1)" json:"score"`
+	Difficulty float64 `gorm:"column:F_difficulty;" json:"difficulty"`
 }
 
 //问题的知识点
@@ -109,24 +112,47 @@ type SmallQuestion struct {
 	//Keypoints     []Keypoint `gorm:"many2many:t_keypoint_question;" json:"keypoints"`        //考察的知识点
 }
 
-func GetQuestion(resId int64) (isBig bool, data interface{}) {
+func GetQuestion(resId int64, q string) (isBig bool, data interface{}) {
 	isBig, _ = GetQuestionTranslateTypeById(resId)
-
 	db := GetDb()
 
-	if isBig {
-		// 大题
-		var bigQuestion BigQuestion
-		db.Table("t_large_questions").Where("F_big_question_id = ?", resId).Scan(&bigQuestion)
-		data = bigQuestion
+	if resId != 0 {
+		if isBig {
+			// 大题
+			var bigQuestion BigQuestion
+			db.Table("t_large_questions").Where("F_big_question_id = ?", resId).Scan(&bigQuestion)
+			data = bigQuestion
+		} else {
+			//小题
+			var smallQuestion SmallQuestion
+			db.Table("t_questions").Where("F_question_id = ?", resId).Scan(&smallQuestion)
+			smallQuestion.RealType = MapTypeSmallQuestion(smallQuestion.Type)
+			smallQuestion.Options = generateOptions(smallQuestion.Accessories, smallQuestion.RealType)
+			smallQuestion.RealAnswer = generateAnswer(smallQuestion.CorrectAnswer, smallQuestion.RealType)
+			data = smallQuestion
+		}
 	} else {
-		//小题
-		var smallQuestion SmallQuestion
-		db.Table("t_questions").Where("F_question_id = ?", resId).Scan(&smallQuestion)
-		smallQuestion.RealType = MapTypeSmallQuestion(smallQuestion.Type)
-		smallQuestion.Options = generateOptions(smallQuestion.Accessories, smallQuestion.RealType)
-		smallQuestion.RealAnswer = generateAnswer(smallQuestion.CorrectAnswer, smallQuestion.RealType)
-		data = smallQuestion
+		if len(q) != 0 {
+			//到大题表中搜索
+			q = "%" + q + "%"
+			var bigQuestion BigQuestion
+			db.Table("t_large_questions").Where("F_content LIKE ?", q).First(&bigQuestion)
+			if bigQuestion.QuestionId != 0 {
+				isBig = true
+				data = bigQuestion
+			} else {
+				//小题表中搜索
+				var smallQuestion SmallQuestion
+				db.Table("t_questions").Where("F_content LIKE ?", q).Scan(&smallQuestion)
+				if smallQuestion.QuestionId != 0 {
+					smallQuestion.RealType = MapTypeSmallQuestion(smallQuestion.Type)
+					smallQuestion.Options = generateOptions(smallQuestion.Accessories, smallQuestion.RealType)
+					smallQuestion.RealAnswer = generateAnswer(smallQuestion.CorrectAnswer, smallQuestion.RealType)
+				}
+				isBig = false
+				data = smallQuestion
+			}
+		}
 	}
 
 	return
@@ -134,21 +160,23 @@ func GetQuestion(resId int64) (isBig bool, data interface{}) {
 
 func generateAnswer(s string, questionType int) []string {
 	result := make([]string, 0)
-	switch questionType {
-	case RADIO_CHOICE, JUDGE_CHOICE, SUBJECTIVITY_BLANK, QA_BLANK:
-		result = append(result, s)
-	case MULIT_CHOICE, INDETERMINATE_CHOICE:
-		res := strings.Split(s, "-")
-		if len(res) > 0 {
-			for i := range res {
-				result = append(result, res[i])
+	if len(s) > 0 {
+		switch questionType {
+		case RADIO_CHOICE, JUDGE_CHOICE, SUBJECTIVITY_BLANK, QA_BLANK:
+			result = append(result, s)
+		case MULIT_CHOICE, INDETERMINATE_CHOICE:
+			res := strings.Split(s, "-")
+			if len(res) > 0 {
+				for i := range res {
+					result = append(result, res[i])
+				}
 			}
-		}
-	case OBJECTIVELY_BLANK:
-		res := strings.Split(s, "-$-")
-		if len(res) > 0 {
-			for i := range res {
-				result = append(result, res[i])
+		case OBJECTIVELY_BLANK:
+			res := strings.Split(s, "-$-")
+			if len(res) > 0 {
+				for i := range res {
+					result = append(result, res[i])
+				}
 			}
 		}
 	}
@@ -183,7 +211,7 @@ func generateOptions(s string, questionType int) (result []string) {
 							xs := Map123toABC(nowOp)
 							result = append(result, xs)
 						} else {
-							result = append(result, xs)
+							result = append(result, handleContent(xs))
 						}
 						nowOp++
 					}
@@ -192,6 +220,38 @@ func generateOptions(s string, questionType int) (result []string) {
 		}
 	}
 	return result
+}
+
+func handleContent(s string) string {
+	//<tex > </tex>标签的内容中的\换成~@
+	regTex, _ := regexp.Compile(`\[tex.*?](.|\n|\f|\r)*?\[\\*/tex]`)
+	regla, _ := regexp.Compile(`\\+`)
+	a := regTex.FindAllString(s, -1)
+
+	for i := range a {
+		x := a[i]
+		x = strings.Replace(x, "\r", "\\r", -1)
+		x = strings.Replace(x, "\f", "\\f", -1)
+		x = strings.Replace(x, "\n", "\\n", -1)
+
+		n := regla.ReplaceAllString(x, "~@")
+		s = strings.Replace(s, a[i], n, 1)
+	}
+	return s
+}
+
+func handleContentRevert(s string) string {
+	//<tex > </tex>标签的内容中的\换成~@
+	regTex, _ := regexp.Compile(`\[tex.*?](.|\n|\f|\r)*?\[\\*/tex]`)
+	regla, _ := regexp.Compile(`(~@)+`)
+	a := regTex.FindAllString(s, -1)
+
+	for i := range a {
+		x := a[i]
+		n := regla.ReplaceAllString(x, "\\\\")
+		s = strings.Replace(s, a[i], n, 1)
+	}
+	return s
 }
 
 func Map123toABC(i int) string {
@@ -214,4 +274,180 @@ func GetQuestionTranslateTypeById(resId int64) (isBig bool, bigCount int) {
 		bigCount = 0
 	}
 	return
+}
+
+func UpdateQuestion(questionId int64, isBig bool, questionType int, data map[string]interface{}) error {
+	tx := GetDb().Begin()
+
+	if isBig {
+		//改大题表
+		updated := make(map[string]interface{})
+		content := data["content"].(string)
+
+		if len(content) > 0 {
+			updated["F_content"] = content
+		}
+
+		if len(updated) > 0 {
+			err := tx.Table("t_large_questions").Where("F_big_question_id = ?", questionId).Updates(updated).Error
+			if err != nil {
+				tx.Rollback()
+				return errors.New("更新大题失败:" + err.Error())
+			}
+			tx.Commit()
+		}
+	} else {
+		//改小题表
+		updated := make(map[string]interface{})
+
+		content := data["content"].(string)
+
+		if len(content) > 0 {
+			updated["F_content"] = content
+		}
+
+		solution := data["solution"].(string)
+
+		if len(solution) > 0 {
+			updated["F_solution"] = solution
+		}
+
+		score := data["score"].(float64)
+
+		if score != -100 {
+			updated["F_score"] = score
+		}
+
+		difficulty := data["difficulty"].(float64)
+
+		if difficulty != -100 {
+			updated["F_difficulty"] = difficulty
+		}
+
+		//处理 Options
+		options := data["options"].(map[int]string)
+
+		if len(options) > 0 {
+			updated["F_accessories"] = handleContentRevert(makeNewOptions(questionId, options))
+		}
+
+		//处理Answer
+		answers := data["answers"].(map[int]string)
+		anLen := data["an_len"].(int)
+
+		if len(answers) > 0 {
+			updated["F_correct_answer"] = makeNewAnswers(questionId, answers, questionType, anLen)
+		}
+
+		if len(updated) > 0 {
+			//makeARow(questionId,"试题","编辑",updated)
+
+			err := tx.Table("t_questions").Where("F_question_id = ?", questionId).Updates(updated).Error
+			if err != nil {
+				tx.Rollback()
+				return errors.New("更新小题失败:" + err.Error())
+			}
+			tx.Commit()
+		}
+	}
+	return nil
+}
+func makeNewAnswers(questionId int64, answers map[int]string, questionType int, anLen int) string {
+	//查老答案
+	var answersDB []string
+	GetDb().Table("t_questions").Where("F_question_id = ?", questionId).Pluck("F_correct_answer", &answersDB)
+	if len(answersDB) > 0 {
+		ans := answersDB[0]
+
+		switch questionType {
+		case RADIO_CHOICE, JUDGE_CHOICE, QA_BLANK, SUBJECTIVITY_BLANK:
+			return answers[0]
+		case MULIT_CHOICE, INDETERMINATE_CHOICE:
+			res := strings.Split(ans, "-")
+			result := make([]string, 0, len(res))
+			if len(res) > 0 {
+				for i := range res {
+					result = append(result, res[i])
+				}
+			}
+
+			for k, v := range answers {
+				result[k] = v
+			}
+
+			return strings.Join(result, "-")
+		case OBJECTIVELY_BLANK:
+			res := strings.Split(ans, "-$-")
+			result := make([]string, 0, len(res))
+			if len(res) > 0 {
+				for i := range res {
+					result = append(result, res[i])
+				}
+			}
+
+			if len(ans) == 0 {
+				result = make([]string, anLen)
+			}
+
+			for k, v := range answers {
+				result[k] = v
+			}
+			return strings.Join(result, "-$-")
+		}
+	}
+	return ""
+}
+
+type Audio struct {
+	AudioId   string `json:"audioId"`
+	Duration  int    `json:"duration"`
+	AudioType int    `json:"audioType"`
+}
+
+type Option struct {
+	Options    []string `json:"options"`
+	OptionType int      `json:"optionType"`
+}
+
+type Accessories struct {
+	Audio  Audio  `json:"audio"`
+	Option Option `json:"option"`
+}
+
+func makeNewOptions(questionId int64, newOptions map[int]string) string {
+	var result string
+
+	//先把答案查出来
+	var answers []string
+	GetDb().Table("t_questions").Where("F_question_id = ?", questionId).Pluck("F_accessories", &answers)
+
+	if len(answers) > 0 {
+		answer := answers[0]
+		var accessories Accessories
+		jDecoder := json.NewDecoder(strings.NewReader(answer))
+		jDecoder.Decode(&accessories)
+
+		if accessories.Option.OptionType != 0 {
+			ansJson := make(map[string]interface{})
+
+			for k, v := range newOptions {
+				accessories.Option.Options[k] = v
+			}
+
+			ansJson["option"] = accessories.Option
+
+			if accessories.Audio.AudioType != 0 {
+				ansJson["audio"] = accessories.Audio
+			}
+
+			var buffer bytes.Buffer
+			jEncoder := json.NewEncoder(&buffer)
+			jEncoder.SetEscapeHTML(false)
+			jEncoder.Encode(ansJson)
+
+			result = buffer.String()
+		}
+	}
+
+	return result
 }
